880259ba6434bac1e85eae221ecf72ecd80c3573
tudorv91 <tudor.wadv@gmail.com>
2015-05-23 18:57:35 +0200

    fixed

--- Assignment/run.m
+++ Assignment/run.m
@@ -4,8 +4,8 @@ clear
 close all
 addpath('./AI-NN')
 
-pause = 0.2;
-
+% Section used for globals (avoid transmitting all parameters as arguments)
+% All initial game conditions should be specified here (session setup)
 global nnStruct chromosome noRounds viewGameDevelopment pauseTime;
 
 viewGameDevelopment = 1;
@@ -14,7 +14,19 @@ chromosome = gen_chromosome(nnStruct);
 noRounds = 1000;
 pauseTime = 0.1;
 
-player1_handle = @probablisticIntention;
+% The next two variables are function handles for the typesof player. 
+% Since both intention output functions accept the same arguments (in the same
+% order), we can call the handles without distinction later in the code,
+% with the same arguments. 
+
+% E.G. Let's say we want player1 to be an AI NN, 
+% we say: player1_handle = @intentionAI
+% if we want it to be a probabilistic one, we say:
+% player1_handle = @probablisticIntention
+
+player1_handle = @intentionAI;
 player2_handle = @probablisticIntention;
 
+% Let them play one game. Check the Workspace variables at the end to see
+% what's the score.
 [player1_score, player2_score] = playGame(player1_handle, player2_handle);
