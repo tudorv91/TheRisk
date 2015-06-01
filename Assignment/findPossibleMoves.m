4209736ab8aaf246e0071122d1f679af214edfb3
Jessica <jess.91@hotmail.it>
2015-06-01 12:13:27 +0200

    Delete the num2str function and add connCountries

--- Assignment/findPossibleMoves.m
+++ Assignment/findPossibleMoves.m
@@ -3,24 +3,29 @@ function [possibleMoves] = findPossibleMoves(myID,gamestate)
 % moves can be made from a country with only 1 army
 % possibleMoves = [myCountry targetCountry myArmy targetArmy targetOwner]
 
+[~, connCountries]=init();
+
 %% Determine the countries owned by this AI
 myCountries = gamestate(gamestate(:,2)==myID,1);
-
+% disp('myCountries');
+% disp(myCountries);
 %% Determine the possible movements 
 nTargets = 0;
 for i = 1:length(myCountries)
-    connectedCountries = num2str(gamestate(myCountries(i),4));
-    nTargets = nTargets + length(connectedCountries);
+    cout=connCountries(myCountries(i),:);
+    cols = find(cout);
+    nTargets = nTargets + length(cols);
 end
 
 targets = zeros(nTargets,5);
 k = 0;
 
 for i = 1:length(myCountries)
-    connectedCountries = num2str(gamestate(myCountries(i),4));
+        cout=connCountries(myCountries(i),:);
+        connectedCountries = find(cout);
     for j = 1:length(connectedCountries)
         k = k + 1;
-        connectedCountry = str2double(connectedCountries(j));
+        connectedCountry = connectedCountries(j);
         targets(k,:) = [ myCountries(i) connectedCountry ...
             gamestate(myCountries(i),3) gamestate(connectedCountry,3) ...
             gamestate(connectedCountry,2)];
